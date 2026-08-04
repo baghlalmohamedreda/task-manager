5b543403dcd1ec6fdb5889bb975cc442e18e7411
Mohamed Reda Baghlal <baghlalmohamedreda.25@ump.ac.ma>
2026-08-04 14:07:01 +0100

    improve task form validation and styling

--- frontend/src/components/taskform/TaskForm.jsx
+++ frontend/src/components/taskform/TaskForm.jsx
@@ -2,17 +2,29 @@ import { useState } from "react"
 import "./TaskForm.css"
 function TaskForm({onAddtask,editingtask}){
     const [title,setTitle]=useState(editingtask ? editingtask.title : "")
-    function handleclick(){
-        onAddtask(title)
-        setTitle("")
+    const [err,setErr]=useState("")
+    function handleclick(e){
+        e.preventDefault()
+        const titletrim=title.trim()
+        if(titletrim){
+             onAddtask(titletrim)
+             setTitle("")
+             setErr("")
+             return
+        }
+       setErr("vous devez remplir le champ") 
     }
     return(
         <>
         <form className="task-form" action="">
-             <input  type="text"  placeholder="Entrez une tâche..." value={title} onChange={(e)=> setTitle(e.target.value)}/>
-             <button onClick={handleclick}>{editingtask ? "Modifier" : "Ajouter"}</button>
+            <div className="input-container">
+                   <input  type="text"  placeholder="Entrez une tâche..." value={title} onChange={(e)=> {setTitle(e.target.value)
+                setErr("")
+             }}/>
+             {err &&<span  className="err">{err}  </span>}
+            </div>
+             <button type="button" onClick={handleclick}>{editingtask ? "Modifier" : "Ajouter"}</button>
         </form>
-       
         </>
     )
 }
